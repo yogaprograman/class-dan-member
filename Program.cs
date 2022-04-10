24908be6555eb91bf9678cec717d3e136af16a91
yogaprograman <101821276+yogaprograman@users.noreply.github.com>
2022-04-10 20:33:12 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMathod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers numbers = new Numbers();
+
+            Console.WriteLine("Minimun #1: {0}", numbers.FindMinimum(3, 1));
+            Console.WriteLine("Minimun #1: {0}", numbers.FindMinimum(3, 2, 4));
+            Console.WriteLine();
+            Console.WriteLine("Maximum #1: {0}", numbers.FindMaximum(3, 1));
+            Console.WriteLine("Maximum #1: {0}", numbers.FindMaximum(3, 2, 4));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ number.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMathod
+{
+    internal class Numbers
+    {
+        private int number1;
+        private int number2;
+        public int FindMinimum(int number1, int number2)
+        {
+            int result = 0;
+            if (number1 < number2)
+            {
+                result = number1;
+            }
+            else
+            {
+                result = number2;
+            }
+            return result;
+        }
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            float result = 0;
+            if (number1 < number2 && number1 < number3)
+            {
+                result = number1;
+            }
+            else if (number2 < number1 && number2 < number3)
+            {
+                result = number2;
+            }
+            else if (number3 < number1 && number3 < number2)
+            {
+                result = number3;
+            }
+            return (int)result;
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            int result = 0;
+            if (number1 > number2)
+            {
+                result = number1;
+            }
+            else
+            {
+                result = number2;
+            }
+            return result;
+        }
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            float result = 0;
+            if (number1 > number2 && number1 > number3)
+            {
+                result = number1;
+            }
+            else if (number2 > number1 && number2 > number3)
+            {
+                result = number2;
+            }
+            else if (number3 > number1 && number3 > number2)
+            {
+                result = number3;
+            }
+            return (int)result;
+        }
+    }
+}
